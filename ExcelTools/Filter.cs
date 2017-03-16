0205422d5024b18605c49226418b718e991c5bd3
misha_r82 <misha_r82@mail.ru>
2017-03-16 23:50:42 +0300

    реализована работа текстового фильтра

--- ExcelTools/Filter.cs
+++ ExcelTools/Filter.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls.Primitives;
+using ExcelTools.Annotations;
 using Microsoft.Office.Interop.Excel;
 
 namespace ExcelTools
 {
-    public abstract class FilterProto
+    public abstract class FilterProto : INotifyPropertyChanged
     {
-        public FilterProto(Range cells)
+        private bool _enabled;
+
+        public FilterProto()
         {
-            FilterRng = Current.CurRegion.CurRng;
+            FilterRng = Current.CurRegion.ActiveCell;
+            int col = FilterRng.Column - Current.CurRegion.firstCol;
+            Name = Current.CurRegion.ActiveRow.Cells[col].ColName;
+            ColNum = Current.CurRegion.firstCol;
             /*Range region = cells.CurrentRegion;
             int left = ((Range)region.Cells[0,0]).Column;
             ColNum = cells.Column - left;
@@ -28,15 +36,23 @@ namespace ExcelTools
                 FilterRng = ws.get_Range(ws.Cells[top, left], ws.Cells[bottom, right]);
             }*/
         }
-        public bool Enabled { get; set; }
+
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set
+            {
+                if (_enabled == value) return;
+                _enabled = value;
+                OnPropertyChanged();
+                if (_enabled) SetFilter();
+                else RemoveFilter();
+            }
+        }
+
         private int ColNum { get; }
-        public string Name { get
-        { 
-            var cell = (Range) FilterRng.Cells[1, 1];
-            if (cell.Value2 == null) return "";
-            return cell.Value2.ToString();
-        }
-        }
+        public string Name { get; }
+        
         private Range FilterRng { get; set; }
         protected virtual object Criteria { get; }
 
@@ -44,7 +60,17 @@ namespace ExcelTools
         {
             FilterRng.AutoFilter(ColNum, Criteria);
         }
+        public void RemoveFilter()
+        {
+            FilterRng.AutoFilter(ColNum, "");
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
     class StrFilter : FilterProto
     {
@@ -54,7 +80,7 @@ namespace ExcelTools
             get { return string.Format("*{0}*", Patt); }
         }
 
-        public StrFilter(Range cells) : base(cells)
+        public StrFilter(Range cells) : base()
         {
             Patt = "1";
             //SetFilter();
@@ -71,7 +97,7 @@ namespace ExcelTools
             get { return new string[] {string.Format(">={0}", From), string.Format("<={0}", To)}; }
         }
 
-        public NumericFilter(Range cells) : base(cells)
+        public NumericFilter(Range cells) : base()
         {
             From = 23;
             To = 27;
@@ -80,7 +106,7 @@ namespace ExcelTools
     }
     class DateFilter : FilterProto
     {
-        public DateFilter(Range cells) : base(cells)
+        public DateFilter(Range cells) : base()
         {
             {
                 From = DateTime.Now.AddDays(-1);
@@ -106,7 +132,7 @@ namespace ExcelTools
             get { return new string[] { string.Format(">={0}", From), string.Format("<={0}", To) }; }
         }
 
-        public TimeFilter(Range cells) : base(cells)
+        public TimeFilter(Range cells) : base()
         {
             From = new TimeSpan(0);
             To = new TimeSpan(0,23,59,59);
