911882deb62c33c045cbf063c8da4294b2e3dd32
misha <misha@PC>
2017-06-28 20:58:35 +0300

    начало перевода на CellValue

---
+++ ExcelTools/CellValue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelTools.Annotations;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelTools
+{
+    class CellValue : INotifyPropertyChanged
+    {
+        public  enum CellValType {String, Numeric, Date, Time}
+
+        private object _xlVal;
+        public CellValType Type { get; }
+
+        public CellValue(Range rng)
+        {
+            string format = rng.NumberFormat.ToString();
+            _xlVal = rng.Value;
+            if (_xlVal == null)
+            {
+                if (IsNumericFormat(format)) Type = CellValType.Numeric;
+                else if (IsDateFormat(format)) Type = CellValType.Date;
+                else if (IsTimeFormat(format)) Type = CellValType.Time;
+                else Type = CellValType.String;
+            }
+            else
+            {
+                if (_xlVal is string)
+                    Type = CellValType.String;
+                else if (_xlVal is double)
+                {
+                    if (IsTimeFormat(format)) Type = CellValType.Time;
+                    else Type = CellValType.Numeric;
+                }
+                else if (_xlVal is DateTime)
+                    Type = CellValType.Date;
+            }
+        }
+
+        private bool IsTimeFormat(string cellFormat)
+        {
+            return cellFormat.Contains('h');
+        }
+
+        private bool IsNumericFormat(string cellFormat)
+        {
+            if (cellFormat.Contains('0')) return true;
+            if (cellFormat.Contains('#')) return true;
+            if (cellFormat.Contains("%")) return true;
+            return false;
+        }
+        private bool IsDateFormat(string cellFormat)
+        {
+            if (IsTimeFormat(cellFormat)) return false;
+            return cellFormat.Contains('m') || cellFormat.Contains('y') || cellFormat.Contains('d');
+        }
+
+        public object XlVal
+        {
+            get { return _xlVal; }
+            set
+            {
+                if (Equals(value, _xlVal)) return;
+                _xlVal = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string StrVal
+        {
+            get
+            {
+                switch (Type)
+                {
+                        case CellValType.String:
+                        return _xlVal.ToString();
+                }
+                return "";
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
